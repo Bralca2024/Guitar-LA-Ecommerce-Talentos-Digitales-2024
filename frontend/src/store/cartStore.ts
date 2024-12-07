b53034605025d9698fb85d212d674fd2cb7bd369
Jonatan <jonatan.vargasportillo@gmail.com>
2024-12-06 22:10:57 -0300

    se agrego la store que maneja los estados globales del carrito

---
+++ frontend/src/store/cartStore.ts
@@ -0,0 +1,57 @@
+import { create } from "zustand";
+import { CartType } from "../types/type";
+import { CartSchema } from "../schema/productSchema";
+
+type CartState = {
+    cart: CartType[];
+    addToCart: (product: CartType) => void;
+    removeFromCart: (productId: string) => void;
+    clearCart: () => void;
+};
+
+//crear el useCartStore
+export const useCartStore = create<CartState>((set) => ({
+    cart: (() => {
+        try {
+            const storedCart = JSON.parse(localStorage.getItem("cart") || "[]");
+            return CartSchema.array().parse(storedCart);
+        } catch (error) {
+            console.error(error);
+            return [];
+        }
+    })(),
+    addToCart: (product) =>
+        set((state) => {
+            const existingProduct = state.cart.find(
+                (item) => item._id === product._id
+            );
+            let updatedCart;
+            if (existingProduct) {
+                updatedCart = state.cart.map((item) =>
+                    item._id === product._id
+                        ? {
+                            ...item,
+                            quantity: item.quantity + product.quantity,
+                        }
+                        : item
+                );
+            } else {
+                updatedCart = [...state.cart, product];
+            }
+            localStorage.setItem("cart", JSON.stringify(updatedCart));
+            return { cart: updatedCart };
+        }),
+    removeFromCart: (productId) =>
+        set((state) => {
+            const updatedCart = state.cart.filter(
+                (item) => item._id !== productId
+            );
+            localStorage.setItem("cart", JSON.stringify(updatedCart));
+            return { cart: updatedCart };
+        }),
+    clearCart: () =>
+        set(() => {
+            localStorage.removeItem("cart");
+            return { cart: [] };
+        }),
+}));
