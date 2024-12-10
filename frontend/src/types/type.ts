a46d23dc8ae2b3636ee3d2e09ca95e63362a5f61
Jonatan <jonatan.vargasportillo@gmail.com>
2024-12-10 12:00:46 -0300

    se agrego los schemas de saleDetailSchema y orderSchema para usar los types

--- frontend/src/types/type.ts
+++ frontend/src/types/type.ts
@@ -2,8 +2,12 @@ import z from "zod";
 import { ProductSchema } from "../schema/productSchema";
 import { CartSchema } from "../schema/productSchema";
 import { UserSchema } from "../schema/userSchema";
+import { SaleDetailSchema } from "../schema/saleDetailSchema";
+import { OrderSchema } from "../schema/orderSchema";
 
 
 export type ProductType = z.infer<typeof ProductSchema>;
 export type CartType = z.infer<typeof CartSchema>;
 export type UserType = z.infer<typeof UserSchema>;
+export type SaleDetail = z.infer<typeof SaleDetailSchema>;
+export type Order = z.infer<typeof OrderSchema>;
