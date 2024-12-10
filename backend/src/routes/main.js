c94955e340044ba9ef59a343a8d94afe425e30ee
Jonatan <jonatan.vargasportillo@gmail.com>
2024-12-10 11:58:08 -0300

    se agregaron sus rutas

--- backend/src/routes/main.js
+++ backend/src/routes/main.js
@@ -3,6 +3,7 @@ import authRouter from "./authRoutes.js";
 import usersRouter from "./usersRoutes.js";
 import productsRouter from "./productsRoutes.js";
 import paymentRouter from './paymentRoutes.js';
+import ordersRouter from "./ordersRoutes.js";
 
 const mainRouter = Router();
 
@@ -10,6 +11,7 @@ mainRouter.use('/auth', authRouter);
 mainRouter.use('/users', usersRouter);
 mainRouter.use('/products', productsRouter);
 mainRouter.use('/payment', paymentRouter);
+mainRouter.use('/orders', ordersRouter);
 
 
 export default mainRouter;
