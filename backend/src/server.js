6e4218a1aa1d8ffe50adc9c4526a6fda0cf03331
Javier Fernando Cabral <fernandocabraldev@gmail.com>
2024-12-13 15:53:55 -0300

    agrega importación de sendEmail desde emailController

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import fileUpload from 'express-fileupload';
 import cors from 'cors';
 import mainRouter from './routes/main.js';
+import sendEmail from './controllers/emailController.js';
 
 const app = express();
 
